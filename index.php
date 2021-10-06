280e7a3854002f5a0c4694e91fcc06e8b3c9a677
Jose Delfin Austria <analyst@secOps.localdomain>
2021-10-05 22:44:11 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>RESUME</title>
+      </head>
+  <body>
+      <!-- <div class="col-md-6"> -->
+      <img  src="img/id.jpg" class="rounded float-end" alt="..." width="15%">   
+   <br><BR>
+<br>
+    <h1>BIO DATA</h1>
+   
+<br> <BR>
+<br>
+    <div class="p-3 mb-2 bg-primary text-white">Personal Information</div>
+
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Name:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="HAZEL EUNICE N. ARSENAL">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Age:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="21">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Place of Birth:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="ORMOC CITY">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Date of Birth:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="NOVEMBER 01, 1999">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Address:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="KADAOHAN ORMOC CITY">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Contact No.:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="09771104778">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Civil Status:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="SINGLE">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Citizenship:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="FILIPINO">
+    </div>
+    </div>
+   
+    
+<div class="mb-3 row">
+    <label placeholder="inputAddress" class="col-sm-2 col-form-label">Religion:</label>
+    <div class="col-sm-10">
+      <input type="address" class="form-control" placeholder="ROMAN CATHOLIC">
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label placeholder="inputAddress" class="col-sm-2 col-form-label">Email Address:</label>
+    <div class="col-sm-10">
+      <input type="address" class="form-control" placeholder="hazeleunice.arsenal@evsu.edu.ph">
+    </div>
+  </div>
+  
+  <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Father's Name:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="CORNILIO B. ARSENAL JR.">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Occupation:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="FARMER">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Mother's Name:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="PSYCHIE C. NAVARRO">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Occupation:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="HOUSEWIFE">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Guardian's Name:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="PSYCHIE C. NAVARRO">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Relation:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="MOTHER">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Guardian's Contact No.:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="09097920394">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Address:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="KADAOHAN ORMOC CITY">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputAddress" class="col-sm-2 col-form-label">Language or dialect spoken aNd written:</label>
+    <div class="col-sm-10">
+      <input type="address" class="form-control" placeholder="CEBUANO">
+    </div>
+  </div>
+   
+
+    
+    <div class="p-3 mb-2 bg-primary text-white">Educational Background</div>
+  
+  <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Elementary:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="CABATOAN ELEMENTARY SCHOOL">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Year graduated:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="2011-2012">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">High School:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="WESTERN LEYTE COLLEGE">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Year graduated:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="2017-2018">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">College:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="EASTERN VISAYAS STATE UNIVERSITY">
+</div>  
+      <label placeholder="inputHighSchool" class="col-sm-2 col-form-label">Year graduated:</label>
+    <div class="col-sm-4">
+      <input type="highschool" class="form-control" placeholder="1018-PRESENT">
+    </div>
+    </div>
+    <div class="mb-3 row">
+    <label placeholder="inputElementary" class="col-sm-2 col-form-label">Degree Received:</label>
+    <div class="col-sm-10">
+      <input type="elementary" class="form-control" placeholder="N/A">
+    </div>
+  </div>
+    
+    <div class="p-3 mb-2 bg-primary text-white">Skills</div>
+    <div class="p-3 mb-2 bg-secondary text-white">Computer Skills<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+Communication Skills<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+Writting Skills<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+Competitiveness<div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div></div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+
+    
+
+  </body>
+</html>
